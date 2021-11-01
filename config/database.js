1730ecbecb952db930fbbbc45e7fe9919f940955
ciaraweston <cweston0727@outlook.com>
2021-11-01 01:05:36 -0400

    second push of demoday

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+  'url': 'mongodb+srv://cweston0727:RIpcpqZAFy0W9qQu@cluster0.ahgdx.mongodb.net/demo?retryWrites=true&w=majority',
+  'dbName': 'plnr'
+};
